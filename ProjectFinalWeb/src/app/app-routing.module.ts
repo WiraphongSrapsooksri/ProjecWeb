eb6cc8bb0d2bbf9f83d08776c0f73a6a0698a2ae
Supakit <64011212100@msu.ac.th>
2023-03-14 19:03:40 +0700

    add login

--- ProjectFinalWeb/src/app/app-routing.module.ts
+++ ProjectFinalWeb/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './customer/main/main.component';
+import { LoginComponent } from './page/login/login.component';
 
 const routes: Routes = [
-  {path:'',component:MainComponent}
+  {path:'',component:LoginComponent}
 ];
 
 @NgModule({
--- ProjectFinalWeb/src/app/app.module.ts
+++ ProjectFinalWeb/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './page/login/login.component';
 import { MainComponent } from './customer/main/main.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainComponent
+    MainComponent,
+
   ],
   imports: [
     BrowserModule,
