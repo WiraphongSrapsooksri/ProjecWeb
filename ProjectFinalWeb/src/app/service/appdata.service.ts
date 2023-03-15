72a06fec3a3dc537c9b7451253c1032320da17cc
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-03-15 17:39:21 +0700

    ad

--- ProjectFinalWeb/src/app/service/appdata.service.ts
+++ ProjectFinalWeb/src/app/service/appdata.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class Dataservice {
-  apiEndpoint = 'http://localhost/work02';
+  apiEndpoint = 'http://localhost:9999/webAPI_ProjectWebFinal';
   constructor() { }
 }
